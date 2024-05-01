71d3f086d668496d6156266fe64c33ab4e1e2df4
kyh1126 <kyh901126@nate.com>
2024-05-01 14:52:33 +0900

    first commit

---
+++ 6_string_processing/1_valid_palindrome.py
@@ -0,0 +1,23 @@
+import re
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        target = "".join(re.findall("[\dA-Za-z]*", s.lower()))
+        splitSeq = len(target) // 2
+
+        if len(target) % 2 == 0:
+            s2 = target[splitSeq:]
+        else:
+            s2 = target[splitSeq + 1:]
+
+        return target[:splitSeq] == s2[::-1]
+
+
+s = Solution()
+# true
+print(s.isPalindrome("A man, a plan, a canal: Panama"))
+# false
+print(s.isPalindrome("race a car"))
+# true
+print(s.isPalindrome(" "))
